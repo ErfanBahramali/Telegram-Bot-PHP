1d9ef6e640d7a4ae02fecdde415dcb42439d0914
Erfan Bahramali <ErfanBahramali@gmail.com>
2021-11-06 00:41:33 +0330

    Add 'ChatMemberStatus' Constant

---
+++ src/constants/ChatMemberStatus.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the TelegramBotPHP package.
+ * https://github.com/ErfanBahramali/Telegram-Bot-PHP
+ */
+
+namespace TelegramBotPHP\constants;
+
+/**
+ * @link https://core.telegram.org/bots/api#chatmember
+ */
+class ChatMemberStatus
+{
+    /**
+     * Creator Chat Member Status ( ChatMemberOwner )
+     * @link https://core.telegram.org/bots/api#chatmemberowner
+     */
+    public const CREATOR = 'creator';
+
+    /**
+     * Administrator Chat Member Status ( ChatMemberAdministrator )
+     * @link https://core.telegram.org/bots/api#chatmemberadministrator
+     */
+    public const ADMINISTRATOR = 'administrator';
+
+    /**
+     * Member Chat Member Status ( ChatMemberMember )
+     * @link https://core.telegram.org/bots/api#chatmembermember
+     */
+    public const MEMBER = 'member';
+
+    /**
+     * Restricted Chat Member Status ( ChatMemberRestricted )
+     * @link https://core.telegram.org/bots/api#chatmemberrestricted
+     */
+    public const RESTRICTED = 'restricted';
+
+    /**
+     * Left Chat Member Status ( ChatMemberLeft )
+     * @link https://core.telegram.org/bots/api#chatmemberleft
+     */
+    public const LEFT = 'left';
+
+    /**
+     * Kicked Chat Member Status ( ChatMemberBanned )
+     * @link https://core.telegram.org/bots/api#chatmemberbanned
+     */
+    public const KICKED = 'kicked';
+}
